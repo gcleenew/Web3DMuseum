8bd4b4d8a39be86ce4531bae000fa6e1287cfc42
gcleenew <cleenewerckguillaume@gmail.com>
2015-05-13 09:13:01 +0200

    débug des liens du header admin

--- app/controllers/BackOffice.java
+++ app/controllers/BackOffice.java
@@ -12,50 +12,50 @@ public class BackOffice extends Controller {
     }
 
     public static Result searchObject() {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result searchObjectResult() {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result objet(Integer id) {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result addObjet() {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result modifyText() {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result searchAdd() {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result searchAddResult() {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result feedback() {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result listParcours() {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result parcours(Integer id) {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result stats() {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 
     public static Result users() {
-        return ok(index.render("This is the header !!!!!", "This is the body !!!!!"));
+        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
     }
 }
