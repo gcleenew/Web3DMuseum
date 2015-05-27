a277eccfbf54ad3b0c31b3b931c79bbe67907607
gcleenew <cleenewerckguillaume@gmail.com>
2015-05-27 12:27:57 +0200

    AJout propose modification et create field et commentaire

---
+++ app/core/CreateFields.java
@@ -0,0 +1,37 @@
+package core;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.Date;
+import java.util.*;
+import java.text.*;
+import models.*;
+
+
+public class CreateFields {
+	
+	public static String checkPropose(String champ, String valeur, Utilisateur utilisateur, Objet objet1) {
+
+
+	    if (valeur != null) {
+	    	if (!valeur.equals( objet1.getField(champ) )) {
+	    		PropositionModification propose = new PropositionModification();
+	    		propose.nomChamp = champ;
+	    		propose.nouveauContenu = valeur;
+	    		propose.utilisateur = utilisateur;
+	    		propose.objet = objet1;
+
+	    		propose.save();
+	    		return "La proposition de "+champ+" a été créée, ";
+	    	}
+	    	else {
+	    		return "";
+	    	}
+	    }
+	    else {
+	    	return "";
+	    }
+	    
+	}
+}
