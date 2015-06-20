ee4c24a28f7495dc8a2ce3196f2384e2a14911a8
gcleenew <cleenewerckguillaume@gmail.com>
2015-06-20 12:45:06 +0200

    Ajout de la modification d'objet, modification des messages d'erreur en flash pour l'ajout d'objet, rajout des liste de fait et ommentaire et suppression et validification de ces derniers

--- app/controllers/BackOffice.java
+++ app/controllers/BackOffice.java
@@ -25,15 +25,130 @@ public class BackOffice extends Controller {
     }
 
     public static Result searchObject() {
-        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
+        DynamicForm requestData = Form.form().bindFromRequest();
+        String reference = requestData.get("reference");
+        List<Objet> objets = Objet.find.all();
+        Objet objet = Objet.find.where().eq("reference", reference).findUnique();
+        String alert = "";
+        if (objet == null ) 
+        {
+            if (reference != "" && reference != null)
+                alert = "<div id='retourRecherche' class='alert alert-danger' role='alert'> Aucun objet sur le site est atribué à cette référence, veuillez vérifier si cette référence à bien été entrée et que l'objet existe bien sur le site. </div>";
+            flash("fail", alert);
+            return ok(searchObject.render(objets));
+        }
+        else {
+            return redirect(controllers.routes.BackOffice.searchObjectResult(objet.id));
+        }   
     }
 
-    public static Result searchObjectResult() {
-        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
+    public static Result searchObjectResult(Integer id) {
+        Objet objet = Objet.find.byId(id);
+        return ok(searchObjectResult.render(objet));
     }
 
     public static Result objet(Integer id) {
-        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
+
+        DynamicForm requestData = Form.form().bindFromRequest();
+        Objet objet1 = Objet.find.byId(id);
+        String alert;
+
+        String nom = requestData.get("nom");
+        String reference = requestData.get("reference");
+        String description = requestData.get("description");
+        String type_objet = requestData.get("type_objet");
+        String matiere = requestData.get("matiere");
+        String stringlarg = requestData.get("largeur");
+        String stringlong = requestData.get("longueur");
+        String stringhaut = requestData.get("hauteur");
+        String stringpoids = requestData.get("poids");
+        Double largeur = 0d;
+        Double longueur = 0d;
+        Double hauteur = 0d;
+        Double poids = 0d;
+        Date dateDecouverte = objet1.dateDecouverte;
+        //parsage des string en double
+        if (stringlarg != null && stringlarg != "") {
+            largeur = Double.parseDouble(stringlarg);
+        }
+        if (stringlong != null && stringlong != "") {
+            longueur = Double.parseDouble(stringlong);
+        }
+        if (stringhaut != null && stringhaut != "") {
+            hauteur = Double.parseDouble(stringhaut);
+        }
+        if (stringpoids != null && stringpoids != "") {
+            poids = Double.parseDouble(stringpoids);
+        }
+
+        // (1) create a SimpleDateFormat object with the desired format.
+        // this is the format/pattern we're expecting to receive.
+
+
+        String expectedPattern = "yyyy-MM-dd";
+        SimpleDateFormat formatter = new SimpleDateFormat(expectedPattern);
+        if (requestData.get("dateDecouverte") != null) {
+            try
+            {
+              // (2) give the formatter a String that matches the SimpleDateFormat pattern
+              String stringdate = requestData.get("dateDecouverte");
+              dateDecouverte = formatter.parse(stringdate);
+            }
+            catch (ParseException e)
+            {
+              // execution will come here if the String that is given
+              // does not match the expected format.
+              e.printStackTrace();
+            }
+        }
+        // remplissage avec les données du formulaire.
+        String localisationActuelle = requestData.get("localisationActuelle");
+        String localisationOrigine = requestData.get("localisationOrigine");
+        String archeologue = requestData.get("archeologue");
+
+        String civilisation = requestData.get("civilisation");
+        String model3D = requestData.get("model3D");
+        // si les trois première informations de l'objet sont remplies alors création de l'objet
+        if (description == null) {
+            
+        }
+        else if (description != "" && description != null) {
+            objet1.description = description;
+            objet1.type_objet = type_objet;
+            objet1.matiere = matiere;
+            objet1.largeur = largeur;
+            objet1.longueur = longueur;
+            objet1.hauteur = hauteur;
+            objet1.poids = poids;
+            objet1.dateDecouverte = dateDecouverte;
+            objet1.localisationActuelle = localisationActuelle;
+            objet1.localisationOrigine = localisationOrigine;
+            objet1.archeologue = archeologue;
+            objet1.civilisation = civilisation;
+            objet1.model3D = model3D;
+
+            objet1.save();
+            alert = "<div id='retourRecherche' class='alert alert-success' role='alert'> L'objet à bien été modifié</div>";
+            flash("sucess", alert);
+        }
+        else {
+            alert = "<div id='retourRecherche' class='alert alert-danger' role='alert'> Veuillez vérifier que le champ description n'as pas été vidé.</div>";
+            flash("fail", alert);
+        }
+
+
+
+        return ok(objet.render(objet1));
+    }
+
+    public static Result deleteObjet(Integer id) {
+
+        Objet objet = Objet.find.byId(id);
+
+        objet.delete();
+        String message = "<div id='retour' class='alert alert-success' role='alert'> Objet supprimé </div>";
+        flash("delete", message);
+        return ok("delete");
     }
 
     public static Result addObjet() {
@@ -50,10 +165,10 @@ public class BackOffice extends Controller {
         String stringlong = requestData.get("longueur");
         String stringhaut = requestData.get("hauteur");
         String stringpoids = requestData.get("poids");
-        Double largeur = 3.1415926535;
-        Double longueur = 3.1415926535;
-        Double hauteur = 3.1415926535;
-        Double poids = 3.1415926535;
+        Double largeur = 0d;
+        Double longueur = 0d;
+        Double hauteur = 0d;
+        Double poids = 0d;
         Date dateDecouverte = new Date();
         //parsage des string en double
         if (stringlarg != null && stringlarg != "") {
@@ -97,7 +212,10 @@ public class BackOffice extends Controller {
         String civilisation = requestData.get("civilisation");
         String model3D = requestData.get("model3D");
         // si les trois première informations de l'objet sont remplies alors création de l'objet
-        if (nom != "" && nom != null && reference != "" && reference != null && description != "" && description != null) {
+        if (description == null) {
+            
+        }
+        else if (nom != "" && nom != null && reference != "" && reference != null && description != "" && description != null) {
             Objet objet = new Objet();
             objet.nom = nom;
             objet.reference = reference;
@@ -117,14 +235,17 @@ public class BackOffice extends Controller {
             objet.model3D = model3D;
 
             objet.save();
-            message = "L'objet : "+nom+" a été crée";
+
+            message = "<div id='retourRecherche' class='alert alert-success' role='alert'> L'objet à bien été crée</div>";
+            flash("success", message);
         }
         else {
-            message = "Vous n'avez pas remplis les trois premiers champs";
+            message = "<div id='retourRecherche' class='alert alert-danger' role='alert'> Les trois premiers champs n'ont pas été remplis.</div>";
+            flash("fail", message);
         }
         
 
-        return ok(addObjet.render("This is the header !!!!!", message));
+        return ok(addObjet.render("Ajout d'objet"));
     }
 
     public static Result modifyText() {
@@ -155,16 +276,64 @@ public class BackOffice extends Controller {
         return ok(modifyText.render(alert, select));
     }
 
-    public static Result searchAdd() {
-        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
+    public static Result listFaits() {
+        List<FaitHistorique> faits = FaitHistorique.find.all();
+        return ok(listFaits.render(faits));
     }
 
-    public static Result searchAddResult() {
-        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
+    public static Result faitHistorique(Long id) {
+        DynamicForm requestData = Form.form().bindFromRequest();
+        FaitHistorique fait = FaitHistorique.find.byId(id);
+        String alert;
+        String action = requestData.get("faitAction");
+
+        if (action != null && action.equals("valid")){
+            fait.valide = true;
+            fait.save();
+            alert = "<div id='retour' class='alert alert-success' role='alert'> Fait Historique validé </div>";
+            flash("valide", alert);
+            return redirect(controllers.routes.BackOffice.listFaits());
+        }
+        else if (action != null && action.equals("delete")){
+            fait.delete();
+            alert = "<div id='retour' class='alert alert-success' role='alert'> Fait Historique supprimé </div>";
+            flash("delete", alert);
+            return redirect(controllers.routes.BackOffice.listFaits());
+        }
+        else {
+
+        }
+        return ok(faitHistorique.render(fait));
     }
 
-    public static Result feedback() {
-        return ok(indexAdmin.render("This is the header !!!!!", "This is the body !!!!!"));
+    public static Result listCommentaires() {
+        List<Commentaire> commentaires = Commentaire.find.all();
+        return ok(listCommentaires.render(commentaires));
+    }
+
+    public static Result commentaire(Long id) {
+        DynamicForm requestData = Form.form().bindFromRequest();
+        Commentaire commentaire1 = Commentaire.find.byId(id);
+        String alert;
+        String action = requestData.get("commentaireAction");
+
+        if (action != null && action.equals("valid")){
+            commentaire1.valide = true;
+            commentaire1.save();
+            alert = "<div id='retour' class='alert alert-success' role='alert'> Commentaire validé </div>";
+            flash("valide", alert);
+            return redirect(controllers.routes.BackOffice.listCommentaires());
+        }
+        else if (action != null && action.equals("delete")){
+            commentaire1.delete();
+            alert = "<div id='retour' class='alert alert-success' role='alert'> Commentaire supprimé </div>";
+            flash("delete", alert);
+            return redirect(controllers.routes.BackOffice.listCommentaires());
+        }
+        else {
+
+        }
+        return ok(commentaire.render(commentaire1));
     }
 
     public static Result listParcours() {
@@ -232,4 +401,6 @@ public class BackOffice extends Controller {
 
         return ok("delete");
     }
+
+
 }
